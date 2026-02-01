920c9edd02ebc72241fa9b9e5605ad7dcfa0b492
Khiriev.MKh <khiriev.rasul@inbox.ru>
2026-02-01 16:54:24 +0500

    added main methods for client and server

---
+++ cmd/client/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
+func main() {
+	printBuildInfo()
+	fmt.Println("Client started!")
+}
+
+func printBuildInfo() {
+	if buildVersion == "" {
+		buildVersion = "N/A"
+	}
+
+	if buildDate == "" {
+		buildDate = "N/A"
+	}
+
+	if buildCommit == "" {
+		buildCommit = "N/A"
+	}
+
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+}
