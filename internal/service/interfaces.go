f8c94d5ba24da14ba1a2d84142581e0f0ef1f891
Khiriev.MKh <khiriev.rasul@inbox.ru>
2026-02-04 19:19:19 +0500

    services: added auth service

--- internal/service/interfaces.go
+++ internal/service/interfaces.go
@@ -1,1 +1,14 @@
 package service
+
+import (
+	"context"
+
+	"github.com/MKhiriev/go-pass-keeper/models"
+)
+
+type AuthService interface {
+	RegisterUser(ctx context.Context, user models.User) (models.User, error)
+	Login(ctx context.Context, user models.User) (models.User, error)
+	CreateToken(ctx context.Context, user models.User) (models.Token, error)
+	ParseToken(ctx context.Context, tokenString string) (models.Token, error)
+}
