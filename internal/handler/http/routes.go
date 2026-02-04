524be7c0f747be20ecb2f85073219ec345b1281f
Khiriev.MKh <khiriev.rasul@inbox.ru>
2026-02-04 19:21:23 +0500

    handlers: added auth routes

--- internal/handler/http/routes.go
+++ internal/handler/http/routes.go
@@ -9,6 +9,12 @@ func (h *Handler) Init() *chi.Mux {
 	router := chi.NewRouter()
 	router.Use(middleware.Recoverer)
 
+	// routes without authorization
+	router.Group(func(r chi.Router) {
+		r.Post("/api/user/register", h.register)
+		r.Post("/api/user/login", h.login)
+	})
+
 	router.MethodNotAllowed(CheckHTTPMethod(router))
 
 	return router
