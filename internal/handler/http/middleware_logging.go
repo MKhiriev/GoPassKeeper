da51b19fab0479555d602bd13c100a8ffa2d16f0
Khiriev.MKh <khiriev.rasul@inbox.ru>
2026-02-05 21:00:15 +0500

    added logging middleware

---
+++ internal/handler/http/middleware_logging.go
@@ -0,0 +1,31 @@
+package http
+
+import (
+	"net/http"
+	"time"
+)
+
+func (h *Handler) WithHashing(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		uri := r.RequestURI
+		method := r.Method
+
+		lw := &responseWriter{
+			ResponseWriter: w,
+		}
+
+		next.ServeHTTP(lw, r)
+
+		duration := time.Since(start)
+
+		h.logger.Info().
+			Str("uri", uri).
+			Str("method", method).
+			Int("status", lw.status).
+			Dur("duration", duration).
+			Int("size", lw.size).
+			Send()
+	})
+}
