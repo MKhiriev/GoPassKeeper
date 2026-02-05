0f24aa75ae43bdfc64b50c6f8db61f85d8033b04
Khiriev.MKh <khiriev.rasul@inbox.ru>
2026-02-05 21:08:01 +0500

    added auth middleware

---
+++ internal/handler/http/errors.go
@@ -0,0 +1,9 @@
+package http
+
+import "errors"
+
+var (
+	ErrEmptyAuthorizationHeader   = errors.New("empty `Authorization` header")
+	ErrInvalidAuthorizationHeader = errors.New("invalid `Authorization` header")
+	ErrEmptyToken                 = errors.New("empty token in `Authorization` header")
+)
