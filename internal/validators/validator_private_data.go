c13219f18371fd08d715ea67d49920135caf4c25
Khiriev.MKh <khiriev.rasul@inbox.ru>
2026-02-12 21:14:06 +0500

    added private data validator methods

--- internal/validators/validator_private_data.go
+++ internal/validators/validator_private_data.go
@@ -1,6 +1,10 @@
 package validators
 
-import "context"
+import (
+	"context"
+
+	"github.com/MKhiriev/go-pass-keeper/models"
+)
 
 type PrivateDataValidator struct {
 }
@@ -10,6 +14,26 @@ func NewPrivateDataValidator() Validator {
 }
 
 func (v *PrivateDataValidator) Validate(ctx context.Context, obj any, fields ...string) error {
+	// TODO implement me!
+	panic("implement me!")
+}
+
+func (v *PrivateDataValidator) validatePrivateData(ctx context.Context, data models.PrivateData) error {
+	// TODO implement me!
+	panic("implement me!")
+}
+
+func (v *PrivateDataValidator) validateUpdateDataRequest(ctx context.Context, request models.UpdateRequest) error {
+	// TODO implement me!
+	panic("implement me!")
+}
+
+func (v *PrivateDataValidator) validateDeleteDataRequest(ctx context.Context, request models.DeleteRequest) error {
+	// TODO implement me!
+	panic("implement me!")
+}
+
+func (v *PrivateDataValidator) validateDownloadDataRequest(ctx context.Context, request models.DownloadRequest) error {
 	// TODO implement me!
 	panic("implement me!")
 }
