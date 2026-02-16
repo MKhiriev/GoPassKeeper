7f59e63744ed24fd1121cf1a20270534f56f4a76
Khiriev.MKh <khiriev.rasul@inbox.ru>
2026-02-16 20:14:18 +0500

    minor fix

--- internal/handler/http/version.go
+++ internal/handler/http/version.go
@@ -7,6 +7,5 @@ import (
 func (h *Handler) getServerVersion(w http.ResponseWriter, r *http.Request) {
 	serverVersion := h.services.AppInfoService.GetAppVersion(r.Context())
 
-	w.Header().Set("Content-Type", "text/plain")
 	w.Write([]byte(serverVersion))
 }
