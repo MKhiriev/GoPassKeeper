a1962963bc2fd160045bbb318c813d67f08650cc
Khiriev.MKh <khiriev.rasul@inbox.ru>
2026-02-22 18:52:57 +0500

    added fields to ciphers table

---
+++ migrations/00004_add_hash_field_to_ciphers.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+
+ALTER TABLE ciphers
+    ADD COLUMN IF NOT EXISTS hash TEXT NOT NULL DEFAULT '';
+
+-- +goose StatementEnd
+
+
+-- +goose Down
+-- +goose StatementBegin
+
+ALTER TABLE ciphers
+    DROP COLUMN IF EXISTS hash;
+
+-- +goose StatementEnd
