5889344c2827e4ef1791a2a474b4eed68361d9ca
Khiriev.MKh <khiriev.rasul@inbox.ru>
2026-02-22 17:24:06 +0500

    added 501 status code `not implemented` for not implemented routes

--- internal/handler/http/auth_settings.go
+++ internal/handler/http/auth_settings.go
@@ -4,15 +4,15 @@ import "net/http"
 
 func (h *Handler) changeUserPassword(w http.ResponseWriter, r *http.Request) {
 	// TODO implement me!
-	panic("implement me")
+	w.WriteHeader(http.StatusNotImplemented)
 }
 
 func (h *Handler) setUserOTP(w http.ResponseWriter, r *http.Request) {
 	// TODO implement me!
-	panic("implement me")
+	w.WriteHeader(http.StatusNotImplemented)
 }
 
 func (h *Handler) deleteUserOTP(w http.ResponseWriter, r *http.Request) {
 	// TODO implement me!
-	panic("implement me")
+	w.WriteHeader(http.StatusNotImplemented)
 }
--- internal/handler/http/sync.go
+++ internal/handler/http/sync.go
@@ -4,10 +4,10 @@ import "net/http"
 
 func (h *Handler) getClientServerDiff(w http.ResponseWriter, r *http.Request) {
 	// TODO implement me!
-	panic("implement me")
+	w.WriteHeader(http.StatusNotImplemented)
 }
 
 func (h *Handler) syncSpecificUserData(w http.ResponseWriter, r *http.Request) {
 	// TODO implement me!
-	panic("implement me")
+	w.WriteHeader(http.StatusNotImplemented)
 }
