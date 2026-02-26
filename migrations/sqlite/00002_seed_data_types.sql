f08cd9ffdf2beea32e4e72d1341d785cbb3d0ce1
Khiriev.MKh <khiriev.rasul@inbox.ru>
2026-02-26 08:16:43 +0500

    added data types to client db

---
+++ migrations/sqlite/00002_seed_data_types.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+INSERT INTO data_types (id, description)
+VALUES
+    (1, 'login_password'),
+    (2, 'text'),
+    (3, 'binary'),
+    (4, 'bank_card')
+ON CONFLICT (id) DO NOTHING;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DELETE FROM data_types WHERE id IN (1, 2, 3, 4);
+-- +goose StatementEnd
