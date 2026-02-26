0e743e041c5bec4221b8b39e6359cd3e7273de05
Khiriev.MKh <khiriev.rasul@inbox.ru>
2026-02-26 19:53:30 +0500

    fix 3: deleted non implemented (yet) grpc stub-handler

--- internal/handler/grpc/doc.go
+++
@@ -1,5 +0,0 @@
-// Package grpc implements the gRPC transport handler layer.
-//
-// It defines the root handler type used to expose service-layer operations
-// through gRPC endpoints and middleware chains.
-package grpc
